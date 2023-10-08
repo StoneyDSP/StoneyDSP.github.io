52ea16e8cce0c13886f05f90f0e17a84caa513e2
StoneyDSP <nathanjhood@googlemail.com>
2023-10-08 04:19:25 +0200

    Added middleware

---
+++ middleware.js
@@ -0,0 +1,27 @@
+import { next } from '@vercel/edge';
+
+export default function middleware(req) {
+
+  // Extract country. Default to US if not found.
+  const country = (req.geo && req.geo.country) || 'US';
+
+  console.log(`Visitor from ${country}`);
+
+  return next({
+    headers: {
+      'Referrer-Policy': 'origin-when-cross-origin',
+      'X-Frame-Options': 'DENY',
+      'X-Content-Type-Options': 'nosniff',
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "authorization, x-client-info, apikey, content-type",
+      "Access-Control-Allow-Methods": "POST, GET, OPTIONS",
+      "X-UA-Compatible": "ie-edge",
+      "Cache-Control": "max-age=10, s-maxage=86400, immutable",
+      "CDN-Cache-Control": "max-age=60",
+      "Vercel-CDN-Cache-Control": "max-age=3600",
+      "X-DNS-Prefetch-Control": "on",
+      "X-XSS-Protection": "1; mode=block",
+      "Strict-Transport-Security": "max-age=63072000; includeSubDomains; preload"
+    },
+  });
+}
